9bd1ae38a93acc585adcac3ff981a950c4024ec9
Alaa-Elnady <alaahamdialnadi@gmail.com>
2024-05-30 00:30:06 +0300

    Adding Personal Details Section in Instructor Page and adding its styling in css file

--- resources/views/instructors/show-instructor-details.blade.php
+++ resources/views/instructors/show-instructor-details.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-  <!-- Header Start -->
+  {{-- Instructor page header --}}
   <div class="container-fluid pageHeroStyle">
     <img class="img-fluid" src="{{ asset('images/instBG.jpg') }}" alt="" style="width: 100%; height:100%; ">
     <div class="position-absolute top-0 start-0 w-100 d-flex align-items-center justify-content-center" style="background: rgba(24, 29, 56, .8); height:575px">
@@ -36,10 +36,54 @@
     </div>
   </div>
 
-  {{-- Instructor Intro --}}
-  <div class="instrucIntroContainer">
-    <h1>About Instructor</h1>
-    <div class="instrucIntroStyle">{{$instructor->introduction}}</div>
+  {{-- Instructor Information Section --}}
+  <div class="instructorInfoSection">
+    {{-- Instructor Intro --}}
+    <div class="instrucIntroContainer">
+      <h1>About Instructor</h1>
+      <div class="instrucIntroStyle">{{$instructor->introduction}}</div>
+    </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 d-flex flex-row align-items-center justify-content-around px-5 py-3 ">
+
+      {{-- Personal details --}}
+      <div class="col-lg-5 col-md-5 col-sm-12 instrucDetailsContainer">
+        <h4 style="padding-left: 30px;">Personal Details</h4>
+        <div class="personalDetailsSection">
+
+          {{-- Phone Number --}}
+          <div class="personalDetailsItem">
+            <div style="font-weight: bold; color: #06bbcc;">Phone Number</div>
+            <div style="width:65%; text-align: left">{{$instructor->phoneNumber}}</div>
+          </div>
+
+          {{-- Email --}}
+          <div class="personalDetailsItem">
+            <div style="font-weight: bold; color: #06bbcc;">Email</div>
+            <div style="width:65%; text-align: left">{{$instructor->email}}</div>
+          </div>
+
+          {{-- Address --}}
+          <div class="personalDetailsItem">
+            <div style="font-weight: bold; color: #06bbcc;">Address</div>
+            <div style="width:65%; text-align: left">{{$instructor->address}}</div>
+          </div>
+
+          {{-- Job Status --}}
+          <div class="personalDetailsItem">
+            <div style="font-weight: bold; color: #06bbcc;">Job Status</div>
+            <div style="width:65%; text-align: left;">{{$instructor->jobStatus}}</div>
+          </div>
+
+        </div>
+      </div>
+
+      {{-- Achivements --}}
+      <div class="col-lg-5 col-md-5 col-sm-12 instrucDetailsContainer">
+        <h4>Achivements</h4>
+      </div>
+
+    </div>
   </div>
 
 </x-layout>
